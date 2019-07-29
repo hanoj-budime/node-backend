b3d473dd0f8d73b6432f0f44ae27e328843ef1ea
HanojBudime <hanoj.budime@shicsha.com>
2019-07-29 14:07:33 +0530

    init commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express();
+const user = require('./routes/user')
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+/*Parse incoming request bodies in a middleware before your handlers, 
+available under the req.body property*/
+const bodyParser = require('body-parser');
+
+
+//mongoose connections
+mongoose.connect(
+    'mongodb+srv://hanoj:' + 
+    process.env.Mongo_Atlas_PW + 
+    '@node-rest-shop-7gyns.mongodb.net/test?retryWrites=true', 
+    {
+    useNewUrlParser : true
+    }
+);
+
+//Handling CORS
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+      return res.status(200).json({});
+    }
+    next();
+  });
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+app.use('/Hello',(req, res, next) =>{
+    res.status(201).json({
+        message : 'Hello world'
+    });
+});
+
+app.use('/user',user);
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,10 @@
+//imports
+const http = require('http');
+const app = require('./app')
+
+const port = process.env.port || 8000;
+
+const server = http.createServer(app);
+
+server.listen(port);
+
