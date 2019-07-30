c3a17b0d4ea2d21a0f191cb6e27c5bb607e2592e
HanojBudime <hanoj.budime@shicsha.com>
2019-07-30 16:55:47 +0530

    code change

--- routes/user.js
+++ routes/user.js
@@ -5,49 +5,62 @@ const bcrypt = require('bcrypt');
 
 
 router.post("/signup", (req, res, next) => {
-    console.log("1");
-    console.log(req.body.email);
-    User.find( { email : { $eq : req.body.email } } )
-        .then(result => {
-            res.status(200).json({
-              result : result,
-              message : "SUCCESS"
-            });
-        }).catch(err => {
-            res.status(500).json(err);
-        })
-    });
+  console.log("1");
+  console.log(req.body.email);
+  User.find({ email: { $eq: req.body.email } })
+    .then(result => {
+      res.status(200).json({
+        result: result,
+        message: "SUCCESS"
+      });
+    }).catch(err => {
+      res.status(500).json(err);
+    })
+});
 
 
 router.post("/login", (req, res, next) => {
-        User.find({ email: req.body.email })
-          .exec()
-          .then(user => {
-            if (user.length < 1) {
-              return res.status(401).json({
-                message: "Auth failed"
-              });
-            }
-            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
-              if (err) {
-                return res.status(401).json({
-                  message: "Auth failed"
-                });
-              }else{
-                return res.status(201).json({
-                    user :user[0],
-                    message: "login"
-                  });
-              }
-            });
-          })
-          .catch(err => {
-            console.log(err);
-            res.status(500).json({
-              error: err
-            });
+  User.find({ email: req.body.email })
+    .exec()
+    .then(user => {
+      if (user.length < 1) {
+        return res.status(401).json({
+          message: "Auth failed"
+        });
+      }
+      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+        if (err) {
+          return res.status(401).json({
+            message: "Auth failed"
           });
-  });
-    
+        } else {
+          return res.status(201).json({
+            user: user[0],
+            message: "login"
+          });
+        }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+//get user 
+router.post("/", (req, res, next) => {
+  console.log(req.body.userId);
+  User.find({ _id:  req.body.userId })
+    .then(result => {
+      res.status(200).json({
+        user: result[0],
+        message: "SUCCESS"
+      });
+    }).catch(err => {
+      res.status(500).json(err);
+    })
+});
 
 module.exports = router;
